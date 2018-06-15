d154806aad7c8dc9b00ec41d785eb0900e6e0744
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-15 14:43:04 +0800

    统一升级新的公众平台插件

--- mp_jfcx.php
+++ mp_jfcx.php
@@ -77,7 +77,12 @@ class plugin_mp_jfcx {
     }
 }
 
+Ecjia_PluginManager::extend('mp_jfcx', function() {
+    require_once RC_Plugin::plugin_dir_path(__FILE__) . 'mp_jfcx.class.php';
+    return new mp_jfcx();
+});
+
 RC_Plugin::register_activation_hook(__FILE__, array('plugin_mp_jfcx', 'install'));
 RC_Plugin::register_deactivation_hook(__FILE__, array('plugin_mp_jfcx', 'uninstall'));
-RC_Hook::add_filter('platform_factory_adapter_instance', array( 'plugin_mp_jfcx', 'adapter_instance' ), 10, 2);
+// RC_Hook::add_filter('platform_factory_adapter_instance', array( 'plugin_mp_jfcx', 'adapter_instance' ), 10, 2);
 // end
