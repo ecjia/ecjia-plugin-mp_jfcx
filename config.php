8cfa69eb72780fc25b2e9046671164f9aafb90ff
ZCC-PC\Zcc <huangyuyuan@ecmoban.comihuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-02 01:25:22 +0800

    恢复配置

--- config.php
+++ config.php
@@ -47,14 +47,12 @@
 defined('IN_ECJIA') or exit('No permission resources.');
 return array(
     'ext_code'      => 'mp_jfcx',
-    
+
     'ext_icon'      => 'images/icon_jfcx.png',
 
     'support_platform' => 'wechat', //仅支持微信公众平台
 
-    'support_platform_type' => ['service', 'unauthorized', 'subscribe', 'test'], //支持微信所有类型公众号
-
-    'support_type'  => Ecjia\App\Platform\Plugin\PlatformAbstract::TypeAdmin | Ecjia\App\Platform\Plugin\PlatformAbstract::TypeMerchant,
+    'support_platform_type' => ['service'], //仅支持微信服务号
 
     'forms' => array(
     ),
